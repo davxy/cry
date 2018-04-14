3bee39b2197f892d387b41a77044c33b3d92a2c6
Davide Galassi <davxy@datawok.net>
2018-04-14 16:18:18 +0200

    Version test fix

--- test/version_test.c
+++ test/version_test.c
@@ -23,8 +23,8 @@
 void version_test(void)
 {
     TRACE("CRY version: %d.%d.%d (%d)\n",
-            CRY_MAJOR, CRY_MINOR, CRY_PATCH, cry_version());
-    TRACE("CRY version (build-time): %d\n", cry_version());
+            CRY_MAJOR, CRY_MINOR, CRY_PATCH, CRY_VERSION);
+    TRACE("CRY version (build-time): %ld\n", cry_version());
     ASSERT(cry_version() == CRY_VERSION);
 }
 
