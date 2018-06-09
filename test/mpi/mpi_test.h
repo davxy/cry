d8cc508a4542a33a630cc5b801cd9676311471be
Davide Galassi <davxy@datawok.net>
2018-06-09 12:25:32 +0200

    MPI (test) : isolated test binary data in a dedicated file

--- test/mpi/mpi_test.h
+++ test/mpi/mpi_test.h
@@ -1,8 +1,20 @@
 /*
- * mpi_test.h
+ * Copyright (c) 2013-2017, Davide Galassi. All rights reserved.
  *
- *  Created on: May 13, 2018
- *      Author: davxy
+ * This file is part of CRY software.
+ *
+ * CRY is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with CRY; if not, see <http://www.gnu/licenses/>.
  */
 
 #ifndef TEST_MPI_MPI_TEST_H_
@@ -10,6 +22,10 @@
 
 #include "test.h"
 #include <cry/mpi.h>
+
+extern const char g_a6400_bin[800];
+extern const char g_b6400_bin[800];
+extern const char g_a6400_b6400_mul_bin[1600];
 
 void mpi_core_test(void);
 void mpi_cmp_test(void);
