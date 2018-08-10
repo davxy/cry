c5106edc7e1065dea7f5209398d15e137e895108
Davide Galassi <davxy@datawok.net>
2018-08-10 14:10:26 +0200

    Base-64 tests from data file

--- test/base64_test.c
+++ test/base64_test.c
@@ -1,19 +1,52 @@
 #include "test.h"
 #include <cry/base64.h>
 
-#define MSG "Hello World"
-#define MLEN strlen(MSG)
-#define ELEN 16
+
+static void encode(int argc, char *argv[])
+{
+    int len;
+
+    ASSERT(argc == 3);
+    len = atoi(argv[2]);
+
+    ASSERT(cry_base64_encode(argv[0], strlen(argv[0]), (char *)g_buf) == len);
+    if (len >= 0)
+        ASSERT_EQ_BUF(g_buf, argv[1], len);
+}
+
+static void decode(int argc, char *argv[])
+{
+    int len;
+
+    ASSERT(argc == 3);
+    len = atoi(argv[2]);
+
+    ASSERT(cry_base64_decode(argv[0], strlen(argv[0]), (char *)g_buf) == len);
+    if (len >= 0)
+        ASSERT_EQ_BUF(g_buf, argv[1], len);
+}
+
+
+static void base64_dispatch(int argc, char *argv[])
+{
+    char *test = *argv;
+
+    argv++;
+    argc--;
+    if (strcmp(test, "encode") == 0)
+        encode(argc, argv);
+    else if (strcmp(test, "decode") == 0)
+        decode(argc, argv);
+    else
+        fprintf(stderr, "Test '%s' not defined\n", test);
+}
+
+
 
 void base64_test(void)
 {
-    char *cbuf = (char *)g_buf;
-
-    ASSERT(cry_base64_encode(MSG, strlen(MSG), cbuf) == ELEN);
-    PRINT_ASC("encoded", cbuf, ELEN);
-    ASSERT_EQ_BUF(cbuf, "SGVsbG8gV29ybGQ=", ELEN);
-
-    ASSERT(cry_base64_decode(cbuf, ELEN, cbuf) == MLEN);
-    PRINT_ASC("decoded", cbuf, MLEN);
-    ASSERT_EQ_BUF(g_buf, MSG, MLEN);
+    fprintf(stderr, "* Base-64\n");
+    func_test("base64_test.data", base64_dispatch);
+    fprintf(stdout, "\n");
 }
+
