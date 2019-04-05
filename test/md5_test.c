2645deb45f5c0830f2c99cddc694bfe855a3ef78
Davide Galassi <davxy@datawok.net>
2019-04-05 22:38:16 +0200

    MD5 RFC 1321 tests fix

--- test/md5_test.c
+++ test/md5_test.c
@@ -16,15 +16,6 @@ static void param_init(struct md5_param *par, int argc, char *argv[])
     raw_init(par->hash, CRY_MD5_DIGEST_SIZE, argv[1]);
 }
 
-static void cry_md5(unsigned char *out, unsigned char *data, size_t len)
-{
-    struct cry_md5_ctx ctx;
-
-    cry_md5_init(&ctx);
-    cry_md5_update(&ctx, data, len);
-    cry_md5_digest(&ctx, out);
-}
-
 static void md5_digest(int argc, char *argv[])
 {
     struct md5_param par;
@@ -50,37 +41,8 @@ static void dispatch(int argc, char *argv[])
         printf("Test '%s' not defined\n", test);
 }
 
-static char *input[] = {
-    "This file is part of CRY software.",
-    "CRY is free software",
-};
-
-static const char md5_hash[] = {
-    0x48,0x6a,0xc6,0x00,0x0e,0xf1,0x6f,0x00,
-    0x12,0x0d,0x02,0x46,0xeb,0x4d,0xa6,0x69
-};
-
-static void test_old(void)
-{
-    struct cry_md5_ctx md5;
-    size_t len, i;
-    unsigned char buf[16];
-
-    cry_md5_init(&md5);
-    for (i = 0; i < ARLEN(input); i++) {
-        len = strlen(input[i]);
-        PRINT_ASC("input", input[i], len);
-        cry_md5_update(&md5, (unsigned char *)input[i], len);
-    }
-    cry_md5_digest(&md5, buf);
-
-    PRINT_HEX("md5", buf, 16);
-    ASSERT_EQ_BUF(buf, md5_hash, 16);
-}
-
 void md5_test(void)
 {
-    test_old();
     printf("* MD5 RFC 1321 Vectors\n");
     func_test("md5_test.data", dispatch);
     printf("\n");
