403790872be37ccdaa8318021fe2b5e256a17a66
Davide Galassi <davxy@datawok.net>
2019-04-22 10:19:28 +0200

    Malloc mock improvement
    Keeping track of the state: IDLE, ACTIVE, FAILED

--- test/malloc_mock.c
+++ test/malloc_mock.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int g_malloc_mock_count = -1;
-int g_malloc_mock_failed = 0;
+int g_malloc_mock_state = MALLOC_MOCK_IDLE;
 
 void *__real_malloc(size_t size);
 void *__real_realloc(void *ptr, size_t size);
@@ -12,38 +12,37 @@ void __real_free(void *ptr);
 
 void *__wrap_malloc(size_t size)
 {
-    //printf("Wrap malloc\n");
-    if (g_malloc_mock_count == 0) {
-        g_malloc_mock_count = -1;
-        g_malloc_mock_failed = 1;
-        return NULL;
-    }
-    if (g_malloc_mock_count > 0)
+    if (g_malloc_mock_state == MALLOC_MOCK_ACTIVE) {
+        if (g_malloc_mock_count == 0) {
+            g_malloc_mock_state = MALLOC_MOCK_FAILED;
+            return NULL;
+        }
         g_malloc_mock_count--;
+    }
     return __real_malloc(size);
 }
 
 void *__wrap_realloc(void *ptr, size_t size)
 {
-    if (g_malloc_mock_count == 0) {
-        g_malloc_mock_count = -1;
-        g_malloc_mock_failed = 1;
-        return NULL;
-    }
-    if (g_malloc_mock_count > 0)
+    if (g_malloc_mock_state == MALLOC_MOCK_ACTIVE) {
+        if (g_malloc_mock_count == 0) {
+            g_malloc_mock_state = MALLOC_MOCK_FAILED;
+            return NULL;
+        }
         g_malloc_mock_count--;
+    }
     return __real_realloc(ptr, size);
 }
 
 void *__wrap_calloc(size_t nmemb, size_t size)
 {
-    if (g_malloc_mock_count == 0) {
-        g_malloc_mock_count = -1;
-        g_malloc_mock_failed = 1;
-        return NULL;
-    }
-    if (g_malloc_mock_count > 0)
+    if (g_malloc_mock_state == MALLOC_MOCK_ACTIVE) {
+        if (g_malloc_mock_count == 0) {
+            g_malloc_mock_state = MALLOC_MOCK_FAILED;
+            return NULL;
+        }
         g_malloc_mock_count--;
+    }
     return __real_calloc(nmemb, size);
 }
 
--- test/malloc_mock.h
+++ test/malloc_mock.h
@@ -1,6 +1,11 @@
 #ifndef MALLOC_MOCK_H_
 #define MALLOC_MOCK_H_
 
+#define MALLOC_MOCK_IDLE    0
+#define MALLOC_MOCK_ACTIVE  1
+#define MALLOC_MOCK_FAILED  2
+
 extern int g_malloc_mock_count;
+extern int g_malloc_mock_state;
 
 #endif /* MALLOC_MOCK_H_ */
