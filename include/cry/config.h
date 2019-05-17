b819becbf43ba8f5deab0ca73511b35dbb7290dd
Davide Galassi <davxy@datawok.net>
2019-05-17 15:15:23 +0200

    Default config file included in the repository

---
+++ include/cry/config.h
@@ -0,0 +1,26 @@
+/*
+ * Automatically generated. Do not edit.
+ */
+
+#define CRY_DEBUG
+#define CRY_COVERAGE
+#define CRY_MPI_INV_EUCLID
+#define CRY_MPI_MUL_COMBA
+#define CRY_MPI_MUL_KARATSUBA
+#define CRY_MPI_MUL_TOOM3
+#define CRY_ECP_MUL_WIN
+#define CRY_ECP_MUL_SLIDING_WIN
+#define CRY_ECP_GRP_SECP192R1_ENABLE
+#define CRY_ECP_GRP_SECP224R1_ENABLE
+#define CRY_ECP_GRP_SECP256R1_ENABLE
+#define CRY_ECP_GRP_SECP384R1_ENABLE
+#define CRY_ECP_GRP_SECP521R1_ENABLE
+#define CRY_ECP_GRP_SECP192K1_ENABLE
+#define CRY_ECP_GRP_SECP224K1_ENABLE
+#define CRY_ECP_GRP_SECP256K1_ENABLE
+#define CRY_ECP_GRP_BP256R1_ENABLE
+#define CRY_ECP_GRP_BP384R1_ENABLE
+#define CRY_ECP_GRP_BP512R1_ENABLE
+#define CRY_MPI_DEBUG_CONF
+#define CRY_RELEASE 190517
+#define CRY_CFLAGS "-MMD -MP -Wall -Wextra -Wpedantic -Wconversion -Wshadow -Wstrict-prototypes -Wmissing-prototypes --coverage -fno-stack-protector -O0 -g3 -fno-inline"
