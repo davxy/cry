ecc4d1c7d108e7f7bd76072b29513bbf5ce805bb
Davide Galassi <davxy@datawok.net>
2019-05-27 15:25:54 +0200

    One more fix in the Makefile

--- include/cry/base64.h
+++ include/cry/base64.h
@@ -6,11 +6,11 @@
 #ifndef CRY_BASE64_H_
 #define CRY_BASE64_H_
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C"{
 #endif
-
-#include <stddef.h>
 
 size_t cry_base64_encode(char *out, const char *in, size_t len);
 
