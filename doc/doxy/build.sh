20bfcee0f520be28a808f3aa728f0bc8668162b1
Davide Galassi <davxy@datawok.net>
2019-10-18 12:26:07 +0200

    Fix in documentation script

--- doc/doxy/build.sh
+++ doc/doxy/build.sh
@@ -3,7 +3,6 @@
 VERSION=$(grep -rnE "define CRY_(MAJOR|MINOR|PATCH)" ../../include/cry/version.h | awk '{ printf("%d", $3); if (NR!=3) printf("."); }')
 echo $VERSION
 
-sed -i "s/\\\$VERSION\\\$/v$VERSION/g" input/home.dox
 sed -i "s/\\\$VERSION\\\$/v$VERSION/g" Doxyfile.in
 
 doxygen Doxyfile.in
